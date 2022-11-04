17edc96bde09fa16c4733bb286106310f8b3827c
EyR1oN <nazarii.hovdysh.knm.2020@lpnu.ua>
2022-11-04 17:31:51 +0200

    updated toDo

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,67 +1,74 @@
 import React from "react";
-import "./App.css";
 import "antd/dist/antd.css";
 import { Button, Input, Typography, List } from "antd";
 import VirtualList from "rc-virtual-list";
 import { useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined, CloseOutlined, CheckOutlined } from "@ant-design/icons";
+import "./styles/style.css";
 
 const { Title } = Typography;
-const ContainerHeight: number = 400;
+
+type ToDoData = {
+  id: number;
+  describe: string;
+}
 
 const App: React.FC = () => {
-  const [data, setData] = useState<string[]>([
-    "first column",
-    "second column",
-    "second column",
-    "second column",
-    "second column",
-    "second column",
-    "second column",
-    "second column",
-    "second column",
-    "second column",
+  const [data, setData] = useState<ToDoData[]>([
+    {id: 1, describe: "first column"},
+    {id: 2, describe: "second column"},
+    {id: 3, describe: "third column"},
+    {id: 4, describe: "fourth column"},
+    {id: 5, describe: "fifth column"},
+    {id: 6, describe: "sixth column"},
+    {id: 7, describe: "seventh column"},
   ]);
   const [toDo, setToDo] = useState<string>("");
 
   return (
-    <div className="App">
-      <div>
-        <Title level={2}>ToDo List</Title>
-        <Input
-          size="middle"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setToDo(e.target.value)
-          }
-          style={{ width: "400px" }}
-          placeholder="add ToDo"
-        />
-        <Button
-          type="primary"
-          size="middle"
-          onClick={(e) => setData([...data, toDo])}
-        >
-          Add
-        </Button>
+    <div className="center-items">
+      <div className="center-cont">
+        <div className="header">
+          <div className="top-25-pr">
+            <Title level={2}>ToDo List</Title>
+            <Input
+              size="middle"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setToDo(e.target.value)
+              }
+              style={{ width: "400px" }}
+              placeholder="add ToDo"
+            />
+            <Button
+              type="primary"
+              size="middle"
+              onClick={(e) => setData([...data, {id: data.length + 1, describe: toDo}])}
+            >
+              Add
+            </Button>
+          </div>
+        </div>
       </div>
-      <div style={{ textAlign: "center" }}>
-        <List>
-          <VirtualList
-            style={{ display: "inline-block", width: "450px" }}
-            data={data}
-            height={ContainerHeight}
-            itemHeight={47}
-            itemKey="item"
-            //onScroll={onScroll}
-          >
-            {(item: string) => (
-              <List.Item key={item.length}>
-                <PlusOutlined onClick={(): void => console.log("sdssds")} />
-                <div>{item}</div>
-              </List.Item>
-            )}
-          </VirtualList>
-        </List>
+
+      <div className="center-cont">
+        <div className="main-cont">
+          <List>
+            <VirtualList
+              className="viral-list"
+              data={data}
+              height={400}
+              itemHeight={47}
+              itemKey="item"
+            >
+              {(item: ToDoData) => (
+                <List.Item key={item.id}>
+                  <PlusOutlined onClick={(): void => console.log(item)} />
+                  <div>{item.describe}</div>
+                </List.Item>
+              )}
+            </VirtualList>
+          </List>
+        </div>
       </div>
     </div>
   );
