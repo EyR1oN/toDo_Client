bed6a91c761e3672a589512e20d8e0d114974111
EyR1oN <nazarii.hovdysh.knm.2020@lpnu.ua>
2022-11-07 18:39:27 +0200

    added connection to api and components

---
+++ todo-client/src/api/categoryApi.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import CategoryModel from "../models/CategoryModel";
+
+const baseURL: string = "https://localhost:44309/";
+
+export async function getCategories(
+  callback: React.Dispatch<React.SetStateAction<CategoryModel[]>>
+): Promise<void> {
+  await axios
+    .get(baseURL + "category")
+    .then(function (response): void {
+      console.log(response.data);
+      callback(response.data);
+    })
+    .catch(function (error): void {
+      console.log(error);
+    });
+}
+
+export async function postToDo(categoryModel: object): Promise<void> {
+  await axios
+    .post(baseURL + "category", categoryModel)
+    .then(function (response): void {
+      console.log(response);
+    })
+    .catch(function (error): void {
+      console.log(error);
+    });
+}
